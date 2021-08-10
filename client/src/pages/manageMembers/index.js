7757a600325681d46c9e5a01dbd59005e0cd37be
kayn.pleiades <kayn.pleiades@gmail.com>
2021-08-10 13:14:09 -0400

    list of current members for the management page

--- client/src/pages/manageMembers/index.js
+++ client/src/pages/manageMembers/index.js
@@ -49,33 +49,29 @@ function ManageMembers() {
         <div className="container-fluid">
             <div className="row d-flex text-center justify-content-center">
                 <div className="col pt-5">
-                    <h1>Add Member</h1>
+                    <h1>Manage Members</h1>
                 </div>
             </div>
             <div className="row d-flex justify-content-center">
-                <div className="col-7">
-                    <div className="table-responsive">
-                        <table className="table table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Member Color</th>
-                                    <th scope="col">Pronouns</th>
-                                    <th scope="col">Content</th>
-                                </tr>
-                            </thead>
-                            <tbody>
+                <div className="col-8">
+                    {members.length ? (
+                        <div className="list-overflow-container">
+                            <ul className="list-group">
                                 {members.map(member => (
-                                    <tr>
-                                        <th scope="row">{member.fullname}</th>
-                                        <td>{member.color1}</td>
-                                        <td>{member.pronouns}</td>
-                                        <td>{member.content}</td>
-                                    </tr>
+                                    <li class="list-group-item d-flex align-items-center">
+                                        <div className="image-parent me-auto p-2">
+                                            <img src={member.image1} className="img-fluid" alt="quixote" />
+                                        </div>
+                                        <h3 className="me-auto p-2">{member.name}</h3>
+                                        <button type="button" className="btn btn-success">Edit</button>
+                                        <button type="button" className="btn btn-danger">Remove</button>
+                                    </li>
                                 ))}
-                            </tbody>
-                        </table>
-                    </div>
+                            </ul>
+                        </div>
+                    ) : (
+                        <h3>No Members</h3>
+                    )}
                 </div>
             </div>
             <div className="row d-flex text-center justify-content-center">
